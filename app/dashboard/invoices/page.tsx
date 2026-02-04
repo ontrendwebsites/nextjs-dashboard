671a2e5eabfd5d689b3989f748ab204b51d9e66c
Andrew McDoougal <andrew@ontrendwebsites.com>
2026-02-04 21:39:55 +1100

    force dynamic

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,6 @@
+export const dynamic = 'force-dynamic'
+
+
 import Pagination from '@/app/ui/invoices/pagination';
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
